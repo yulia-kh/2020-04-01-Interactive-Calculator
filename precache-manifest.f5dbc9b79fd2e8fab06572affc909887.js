ea5dbd5fd035c154230451672da1502e7c05c57f
yulia-kh <y.khisamutdinova@gmail.com>
2020-04-01 11:56:33 -0700

    Updates

---
+++ precache-manifest.f5dbc9b79fd2e8fab06572affc909887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ac460992fa21944e0e7139c46976dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30b6dbbe33fa39761e8e",
+    "url": "/static/css/main.4884f2ba.chunk.css"
+  },
+  {
+    "revision": "cdf16a24581f8aa958a8",
+    "url": "/static/js/2.1cde9867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1cde9867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b6dbbe33fa39761e8e",
+    "url": "/static/js/main.fcf33291.chunk.js"
+  },
+  {
+    "revision": "41ed7f36163a1419075f",
+    "url": "/static/js/runtime-main.6ae2c8b7.js"
+  }
+]);
