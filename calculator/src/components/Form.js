a9bc6be974949eaa79c878ff979b8f4363b72fdf
yulia-kh <y.khisamutdinova@gmail.com>
2020-04-01 10:29:04 -0700

    Style form

--- calculator/src/components/Form.js
+++ calculator/src/components/Form.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
 export default class Form extends React.Component {
+    state = {
+        sleep: '',
+        exercise: '',
+        meals: '',
+        reading: ''
+    }
   render() {
     return (
       <form className="Form">
        <legend>Tell us what you did today</legend>
        <label htmlFor='sleep'>How many hours of sleep did you have today?</label>
-       <input id='sleep'></input>
+       <input type='text' id='sleep'></input>
        <label htmlFor='exercise'>How many minutes of exercise did you have today?</label>
-       <input id='exercise'></input>
+       <input type='text' id='exercise'></input>
        <label htmlFor='meals'>How many meals did you have today?</label>
-       <input id='meals'></input>
+       <input type='text' id='meals'></input>
        <label htmlFor='reading'>How much did you read today?</label>
-       <input></input>
+       <input type='text' id='reading'></input>
+       <button type='submit'>Submit answers</button>
       </form>
     );
   }
--- calculator/src/components/Navbar.js
+++ calculator/src/components/Navbar.js
@@ -4,7 +4,7 @@ export default class Navbar extends React.Component {
   render() {
     return (
       <header className="App-header">
-        <h1>Track you activities! Be selfaware, be cool!</h1>
+        <h1>Track you activities! Be self-aware, be cool!</h1>
       </header>
     );
   }
